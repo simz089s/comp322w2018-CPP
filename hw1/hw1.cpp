3b5286cebcf32b42deaa5433fefbbd342d233ce7
Simon Zheng <simz97@outlook.com>
2018-02-12 12:40:40 -0500

    HW1 Q6 left

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -12,6 +12,7 @@
 const int rows = 5;
 const int cols = 5;
 
+// Q2
 void fillMatrix(int matrix[rows][cols])
 {
     srand(time(NULL));
@@ -24,6 +25,34 @@ void fillMatrix(int matrix[rows][cols])
     }
 }
 
+// Q7
+void fillMatrix(int** matrix)
+{
+    srand(time(NULL));
+    for (int i = 0; i < rows; i++)
+    {
+        *(matrix+i) = new int[cols];
+        for (int j = 0; j < cols; j++)
+        {
+            *(*(matrix+i)+j) = rand() % 10;
+        }
+    }
+}
+
+// Q8
+void fillMatrix(int matrix[rows*cols])
+{
+    srand(time(NULL));
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i*cols + j] = rand() % 10;
+        }
+    }
+}
+
+// Q3
 void PrintMatrix(int matrix[rows][cols])
 {
     for (int i = 0; i < rows; i++)
@@ -36,6 +65,33 @@ void PrintMatrix(int matrix[rows][cols])
     }
 }
 
+// Q7
+void PrintMatrix(int** matrix)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            std::cout << *(*(matrix+i)+j) << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Q8
+void PrintMatrix(int matrix[rows*cols])
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            std::cout << matrix[i*cols + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+// Q4
 void transposeMatrix(int matrix[rows][cols])
 {
     for (int i = 0; i < rows; i++)
@@ -49,24 +105,44 @@ void transposeMatrix(int matrix[rows][cols])
     }
 }
 
-int* multiplyMatrices(int matrix1[rows][cols], int matrix2[rows][cols])
+// Q7
+void transposeMatrix(int** matrix)
 {
-    int matrix3[rows][cols];
     for (int i = 0; i < rows; i++)
     {
         for (int j = i; j < cols; j++)
         {
-            matrix3[i][j] = matrix1[i][j] * matrix2[j][i];
+            int tmp = *(*(matrix+i)+j);
+            *(*(matrix+i)+j) = *(*(matrix+j)+i);
+            *(*(matrix+j)+i) = tmp;
         }
     }
 }
 
+// Q5
+int** multiplyMatrices(int matrix1[rows][cols], int matrix2[rows][cols])
+{
+    int** matrix3 = new int*[rows];
+    for (int i = 0; i < rows; i++)
+    {
+        matrix3[i] = new int[cols];
+        for (int j = 0; j < cols; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < cols; k++)
+            {
+                sum += matrix1[i][k] * matrix2[k][j];
+            }
+            matrix3[i][j] = sum;
+        }
+    }
+    return matrix3;
+}
+
 int main(int argc, char** argv)
 {
-    int matrix[rows][cols];
+    int* matrix = new int[rows*cols];
     fillMatrix(matrix);
-    PrintMatrix(matrix);
-    transposeMatrix(matrix);
     PrintMatrix(matrix);
     return EXIT_SUCCESS;
 }
