e170eace4af6a82acb52f4679c9829923f0d2fee
Simon Zheng <simz97@outlook.com>
2018-02-20 13:48:55 -0500

    HW2 Add header file content as comment in source for easier grading

--- hw2/dlinkedlist.cpp
+++ hw2/dlinkedlist.cpp
@@ -4,6 +4,53 @@
 #include "dlinkedlist.h"
 
 /**
+ * Header file structure:
+
+#ifndef DLINKEDLIST_H
+#define DLINKEDLIST_H
+
+class Node
+{
+public:
+    Node();
+    ~Node();
+    Node(int data, Node* next, Node* previous);
+    int getData();
+    Node* getNext();
+    Node* getPrev();
+    void setData(int data);
+    void setNext(Node* next);
+    void setPrev(Node* previous);
+
+private:
+    int data;
+    Node* next;
+    Node* previous;
+};
+
+class DLLStructure
+{
+public:
+    DLLStructure();
+    ~DLLStructure();
+    DLLStructure(int array[], int length);
+    Node* getHd();
+    Node* getTl();
+    void setHd(Node* head);
+    void setTl(Node* tail);
+    void PrintDLL();
+    void InsertAfter(int valueToInsertAfter, int valueToBeInserted);
+
+private:
+    Node* head;
+    Node* tail;
+};
+
+#endif // DLINKEDLIST_H
+
+*/
+
+/**
  * Node
  */
 
